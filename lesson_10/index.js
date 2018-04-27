a5d1e3a9624ead6ed9fb20d007772f1441219cd5
innabodak <innabodak@ukr.net>
2018-04-28 02:33:04 +0300

    lesson_10

---
+++ lesson_10/index.js
@@ -0,0 +1,19 @@
+const myAge = 1;
+
+let word = "лет";
+
+
+if ((myAge > 10 & myAge < 20) || (myAge > 110 & myAge < 120)) { 
+	word = "лет"
+}
+
+else if (myAge % 10 == 1) {
+	word = "год"
+}
+
+else if ((myAge % 10 >= 2) & (myAge % 10 <= 4)) {
+	word = "года"
+}
+
+console.log(`Мой возраст ${myAge} ${word}`);
+
