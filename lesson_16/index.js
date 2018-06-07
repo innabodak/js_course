899bf5d03b22eb651b8e05efe305daf62cb6376a
innabodak <innabodak@ukr.net>
2018-06-07 14:24:20 +0300

    scale background

--- lesson_16/index.js
+++ lesson_16/index.js
@@ -16,11 +16,18 @@
 
 
 
-// part #1 only!!!
-
 const n = 10;
 const numbers = document.getElementById("numbers");
 
+for (let i = 1; i <= n; i +=1){
+	let number = document.createElement("span");
+	number.appendChild(document.createTextNode(i));
+	numbers.appendChild(number);
+}
+
+for (let k = 1; k < numbers.children.length; k +=1){
+	numbers.insertBefore(document.createTextNode(", "), numbers.children[k]);
+}
 
 const output = document.getElementById("output");
 const start = document.getElementById("start");
@@ -30,14 +37,11 @@ const x1 = document.querySelector("#X1");
 const x2 = document.querySelector("#X2");
 const x3 = document.querySelector("#X3");
 
-
 let time = 0;
 let counter;
 let interval = 1000;
 let speed = interval;
-// let checkStart = false;
-// let checkPause = false;
-
+let checkStart = false;
 
 start.addEventListener("click", timer);
 x1.addEventListener("click", accelerator)
@@ -45,6 +49,8 @@ x2.addEventListener("click", accelerator)
 x3.addEventListener("click", accelerator)
 
 function timer(){
+	checkStart = true;
+
 	counter = setInterval(function(){
 		time += 1;
 		if (time === 1){
@@ -53,8 +59,27 @@ function timer(){
 			output.innerHTML = time + " seconds";
 		}
 
-		// checkStart = true;
-		// checkPause = false;
+		for (let j = 0; j < n; j += 1){
+			if (Math.trunc(time / n) % 2 === 1 
+				&& time % n === 0) {
+				numbers.children[numbers.children.length - 1].style.background = "green";
+
+			} else if (Math.trunc(time / n) % 2 === 0 
+				&& time % n === 0) {
+				numbers.children[j].style.background = "black";
+				console.log(j);
+			
+			} else if (Math.trunc(time / n) % 2 === 0 
+				&& time % n === j) {
+				numbers.children[j - 1].style.background = "green";
+
+			} else if (Math.trunc(time / n) % 2 === 1 
+				&& time % n === j) {
+				numbers.children[numbers.children.length - j].style.background = "black";
+				console.log(j);
+			}
+		}
+
 	}, 	speed);
 
 	start.setAttribute("disabled", true);
@@ -62,7 +87,7 @@ function timer(){
 	pause.addEventListener ("click", () => {
 		clearInterval(counter);
 		start.removeAttribute("disabled");
-		// checkPause = true;
+		checkStart = false;
 
 	});
 	
@@ -71,52 +96,45 @@ function timer(){
 		time = 0;
 		output.innerHTML = time + " seconds";
 		start.removeAttribute("disabled");
-		// checkStart = false;
-		// checkPause = false;
+		checkStart = false;
+
+		for (let i = 0; i < numbers.children.length; i++ ) {
+			numbers.children[i].style.background = "";
+		}
+
 	});
 }
 
 function accelerator(){
-/*	if (checkPause === false
-		 && checkStart === true) {*/
-		switch (true) {
-			case x1.checked: 
-				speed = interval;
+	switch (true) {
+		case x1.checked: 
+			speed = interval;
+			if (checkStart) {
 				clearInterval(counter);
 				timer();
-				break;
+			}
+			break;
 
-			case x2.checked:
-				speed = interval / 2;
+		case x2.checked:
+			speed = interval / 2;
+			if (checkStart) {
 				clearInterval(counter);
 				timer();
-				break;
+			}
+			break;
 
-			case x3.checked:
-				speed = interval / 3;
+		case x3.checked:
+			speed = interval / 3;
+			if (checkStart) {
 				clearInterval(counter);
 				timer();
-				break;
+			}
+			break;
 
-			default:
-				speed = interval;
-		}
-/*	}*/
+		default:
+			speed = interval;
+	}
 }
 
-
-for (let j = 1; j <= n; j +=1){
-	let number = document.createElement("span");
-	number.appendChild(document.createTextNode(j));
-	numbers.appendChild(number);
-	
-	// if (time % 10 === j) {
-	// 	numbers.children[j].style.background = "green";}
-	// else {
-	// 	numbers.children[j].style.background = "black";
-	// }
-}
-
-// К сожалению, на этом все((
 
  
