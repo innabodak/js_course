a4491bbfed3a667c5b068d51f11b4a386ef4e062
innabodak <innabodak@ukr.net>
2018-06-21 23:31:27 +0300

    regexp

--- lesson_24/index.js
+++ lesson_24/index.js
@@ -1,94 +1,94 @@
-// // ==============================================================
-// // ВАРИАНТ ЧЕРЕЗ ФУНКЦИИ
+// ==============================================================
+// ВАРИАНТ ЧЕРЕЗ ФУНКЦИИ
 
-// const inpEmail = document.getElementById("email");
-// const inpPhone = document.getElementById("phone");
-// const emailError = document.querySelector('label[for="email"]');
-// const phoneError = document.querySelector('label[for="phone"]');
-// // textarea
-// const content = document.getElementById("content");
-// const emailListBtn = document.getElementById("search");
-// const list = document.querySelector("ol");
-// const noEmailFound = document.getElementById("noEmailFound");
-// // validation
-// const phoneCheck = /^\+38(\(0\d\d\))|(0\d\d)\d{3}[ _]?\d{2}[ _]?\d{2}$/;
-// // const emailCheck = /^[\w][\w\D]{2,}[\w]\@(([a-z\d][\w-]+[a-z\d])|(\[[A-z\d\:\.]+\]))\.[a-z]{2,4}$/;
-// const emailCheck = /^[\w][\w-\.]{2,}[\w]\@(([a-z\d][\w-]+[a-z\d])|(\[[A-z\d\:\.]+\]))\.[a-z]{2,4}$/;
-// const textEmailsCheck = /[\w][\w-\.]{2,}[\w]\@(([a-z\d][\w-]+[a-z\d])|(\[[A-z\d\:\.]+\]))\.[a-z]{2,4}/;
+const inpEmail = document.getElementById("email");
+const inpPhone = document.getElementById("phone");
+const emailError = document.querySelector('label[for="email"]');
+const phoneError = document.querySelector('label[for="phone"]');
+// textarea
+const content = document.getElementById("content");
+const emailListBtn = document.getElementById("search");
+const list = document.querySelector("ol");
+const noEmailFound = document.getElementById("noEmailFound");
+// validation
+const phoneCheck = /^\+38(\(0\d\d\))|(0\d\d)\d{3}[ _]?\d{2}[ _]?\d{2}$/;
+// const emailCheck = /^[\w][\w\D]{2,}[\w]\@(([a-z\d][\w-]+[a-z\d])|(\[[A-z\d\:\.]+\]))\.[a-z]{2,4}$/;
+const emailCheck = /^[\w][\w-\.]{2,}[\w]\@(([a-z\d][\w-]+[a-z\d])|(\[[A-z\d\:\.]+\]))\.[a-z]{2,4}$/;
+const textEmailsCheck = /[\w][\w-\.]{2,}[\w]\@(([a-z\d][\w-]+[a-z\d])|(\[[A-z\d\:\.]+\]))\.[a-z]{2,4}/;
 
-// inpEmail.addEventListener("blur", () =>{
-// 	if (!inpEmail.value){
-// 		inpEmail.style.border = "2px solid grey";
-// 		emailError.style.display = "none";
+inpEmail.addEventListener("blur", () =>{
+	if (!inpEmail.value){
+		inpEmail.style.border = "2px solid grey";
+		emailError.style.display = "none";
 
-// 	} else if(emailCheck.test(inpEmail.value)){
-// 		inpEmail.style.border = "2px solid limegreen";
-// 		emailError.style.display = "none";
+	} else if(emailCheck.test(inpEmail.value)){
+		inpEmail.style.border = "2px solid limegreen";
+		emailError.style.display = "none";
 
-// 	} else {
-// 		inpEmail.style.border = "2px solid red";
-// 		emailError.style.display = "block";
-// 	}
-// });
+	} else {
+		inpEmail.style.border = "2px solid red";
+		emailError.style.display = "block";
+	}
+});
 
-// inpPhone.addEventListener("blur", () =>{
-// 	if (!inpPhone.value){
-// 		inpPhone.style.border = "2px solid grey";
-// 		phoneError.style.display = "none";
+inpPhone.addEventListener("blur", () =>{
+	if (!inpPhone.value){
+		inpPhone.style.border = "2px solid grey";
+		phoneError.style.display = "none";
 
-// 	} else if(phoneCheck.test(inpPhone.value)){
-// 		inpPhone.style.border = "2px solid limegreen";
-// 		phoneError.style.display = "none";
+	} else if(phoneCheck.test(inpPhone.value)){
+		inpPhone.style.border = "2px solid limegreen";
+		phoneError.style.display = "none";
 
-// 	} else 	{
-// 		inpPhone.style.border = "2px solid red";
-// 		phoneError.style.display = "block";
-// 	}
-// });
+	} else 	{
+		inpPhone.style.border = "2px solid red";
+		phoneError.style.display = "block";
+	}
+});
 
-// emailListBtn.addEventListener("click", () => {
-// 	let str = content.value;
-// 	let step, cutStr;
-// 	let result = {};
-// 	for (let i = 0; i < str.length; i++) {
-// 		if (textEmailsCheck.exec(str)) {
-// 			result[textEmailsCheck.exec(str)[0]] = true;
-// 			step = textEmailsCheck.exec(str).index + textEmailsCheck.exec(str)[0].length;
-// 			cutStr = str.split("").splice(step).join("");
-// 			str = cutStr;
-// 		} else {
-// 			break;
-// 		}	
-// 	}
+emailListBtn.addEventListener("click", () => {
+	let str = content.value;
+	let step, cutStr;
+	let result = {};
+	for (let i = 0; i < str.length; i++) {
+		if (textEmailsCheck.exec(str)) {
+			result[textEmailsCheck.exec(str)[0]] = true;
+			step = textEmailsCheck.exec(str).index + textEmailsCheck.exec(str)[0].length;
+			cutStr = str.split("").splice(step).join("");
+			str = cutStr;
+		} else {
+			break;
+		}	
+	}
 	
-// 	let arr = Object.keys(result);
-// 	if (arr.length === 0) {
-// 		noEmailFound.style.display = "block";
+	let arr = Object.keys(result);
+	if (arr.length === 0) {
+		noEmailFound.style.display = "block";
 
-// 	} else {
-// 		noEmailFound.style.display = "none";
-// 	}
+	} else {
+		noEmailFound.style.display = "none";
+	}
 
-// 	for (let j = 0; j < arr.length; j++){
-// 		const li = document.createElement("li");
-// 		li.setAttribute("id", arr[j]);
-// 		li.textContent = arr[j];
-// 		list.appendChild(li);
-// 	}
-// 	emailListBtn.setAttribute("disabled", true);
-// });
+	for (let j = 0; j < arr.length; j++){
+		const li = document.createElement("li");
+		li.setAttribute("id", arr[j]);
+		li.textContent = arr[j];
+		list.appendChild(li);
+	}
+	emailListBtn.setAttribute("disabled", true);
+});
 
-// content.addEventListener("focus", () => {
-// 	noEmailFound.style.display = "none";
-// 	arrOfLi = document.querySelectorAll("li");
-// 	if (this.arrOfLi.length) {
-// 		for (let i = 0; i < arrOfLi.length; i++) {
-// 			list.removeChild(arrOfLi[i]);
-// 		}
-// 	}
+content.addEventListener("focus", () => {
+	noEmailFound.style.display = "none";
+	arrOfLi = document.querySelectorAll("li");
+	if (this.arrOfLi.length) {
+		for (let i = 0; i < arrOfLi.length; i++) {
+			list.removeChild(arrOfLi[i]);
+		}
+	}
 
-// 	emailListBtn.removeAttribute("disabled");
-// });
+	emailListBtn.removeAttribute("disabled");
+});
 
 // ==============================================================
 // ВАРИАНТ ЧЕРЕЗ КЛАССЫ
